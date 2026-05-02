a8dc262024d8d78765da2d4185107b16e22de14b
Grigo <danielegrigore@icloud.com>
2026-05-02 19:02:49 +0200

    Primo commit - setup Laravel

---
+++ app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Spatie\Permission\Traits\HasRoles;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+
+class User extends Authenticatable
+{
+    use HasRoles;
+    use HasApiTokens;
+    use Notifiable;
+   
+}
